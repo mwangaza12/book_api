006c3038a1dcacf8b94ed13a947c5b10133b2da1
mwangaza12 <josephmwangaza1@gmail.com>
2025-06-12 13:44:48 +0300

    neon database config

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,19 +1,13 @@
-import "dotenv/config"
-import { Client } from "pg"
-import {drizzle} from "drizzle-orm/node-postgres";
-import * as schema from "./schema"
- 
- 
-export const client = new Client({
-    connectionString: process.env.DATABASE_URL as string
-});
- 
-const main = async () =>{
-    await client.connect(); //connect to the database  
+import "dotenv/config";
+import { neon } from "@neondatabase/serverless"
+import { drizzle } from "drizzle-orm/neon-http"
+import * as schema from  "./schema";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
 }
- 
-main().catch(console.error)
- 
-const db = drizzle(client,{schema, logger:true});
+
+const client = neon(process.env.DATABASE_URL);
+const db = drizzle(client, { schema, logger: true });
+
 export default db;
- 
